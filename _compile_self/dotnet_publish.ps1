c71c07cd7a6b71e07eda26f9792cbe4ca005685f
hsakoh <20980395+hsakoh@users.noreply.github.com>
2025-12-18 05:00:40 +0900

    Changed to self-contained deployment.

--- _compile_self/dotnet_publish.ps1
+++ _compile_self/dotnet_publish.ps1
@@ -1,5 +1,5 @@
 dotnet restore "src/SwitchBotMqttApp/SwitchBotMqttApp.csproj"
 dotnet clean "src/SwitchBotMqttApp/SwitchBotMqttApp.csproj" -c Release
-dotnet publish "src/SwitchBotMqttApp/SwitchBotMqttApp.csproj" -r linux-musl-arm64 -p:PublishSingleFile=true --no-self-contained -c Release -o "./_compile_self/aarch64" --no-restore
-dotnet publish "src/SwitchBotMqttApp/SwitchBotMqttApp.csproj" -r linux-musl-x64 -p:PublishSingleFile=true --no-self-contained -c Release -o "./_compile_self/amd64" --no-restore
-dotnet publish "src/SwitchBotMqttApp/SwitchBotMqttApp.csproj" -r linux-musl-arm -p:PublishSingleFile=true --no-self-contained -c Release -o "./_compile_self/armv7" --no-restore
+dotnet publish "src/SwitchBotMqttApp/SwitchBotMqttApp.csproj" -r linux-musl-arm64 -p:PublishSingleFile=true --self-contained -c Release -o "./_compile_self/aarch64" --no-restore
+dotnet publish "src/SwitchBotMqttApp/SwitchBotMqttApp.csproj" -r linux-musl-x64 -p:PublishSingleFile=true --self-contained -c Release -o "./_compile_self/amd64" --no-restore
+dotnet publish "src/SwitchBotMqttApp/SwitchBotMqttApp.csproj" -r linux-musl-arm -p:PublishSingleFile=true --self-contained -c Release -o "./_compile_self/armv7" --no-restore
